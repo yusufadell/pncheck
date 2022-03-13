2fe0aec25a9e88414002691806c65682a5eab96c
yusufadell <youseefadel777@gmail.com>
2022-03-13 10:59:35 +0200

    Debug Flase for production

--- pncheck/settings.py
+++ pncheck/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#sbku8sk=z_s^ka)jumu#ntw2=(w6beoq13_(r-_4nb5_#j$kp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
